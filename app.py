f00d81cbec32143d4ed5d7326d4c01aa5e2fec8e
Anshuman042005 <sikdaranshuman@gmail.com>
2026-04-29 22:17:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("AI Resume Analyzer Project")
+def analyze_resume(resume_text):
+    print("\n--- AI Resume Analysis ---\n")
+
+    print("Suggested Roles:")
+    print("- Data Analyst")
+    print("- AI/ML Intern")
+
+    print("\nSkill Gaps:")
+    print("- Machine Learning Projects")
+    print("- Advanced Python Libraries")
+
+    print("\nSuggestions:")
+    print("- Add more AI/ML projects")
+    print("- Include GitHub portfolio")
+    print("- Improve technical depth")
+
+resume = input("Paste your resume here:\n")
+analyze_resume(resume)
